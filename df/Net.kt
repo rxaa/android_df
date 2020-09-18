f5221d8fcbb99540885d93d284d50a068602a11d
rxaa <zyxdde@gamil.com>
2020-09-19 02:54:37 +0800

    getAllNet

--- df/Net.kt
+++ df/Net.kt
@@ -1,6 +1,7 @@
 package rxaa.df
 
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import android.os.Build
 
@@ -18,6 +19,44 @@ object Net {
         val cont = df.appContext ?: return false
         val net = cont.getConnectivityManager().activeNetworkInfo ?: return false
         return net.isConnected && net.type == ConnectivityManager.TYPE_WIFI
+    }
+
+    @JvmStatic
+    inline fun getAllCapabilities(func: (net: NetworkCapabilities) -> Unit) {
+        val cont = df.appContext ?: return
+        val netMana = cont.getConnectivityManager();
+        for (net in netMana.allNetworks) {
+            val ca = netMana.getNetworkCapabilities(net) ?: continue;
+            func(ca);
+        }
+    }
+
+    fun isNetworkConnected(): Boolean {
+        val context = df.appContext ?: return false;
+        var result = false
+        val cm = context.getConnectivityManager() ?: return false;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    result = true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    result = true
+                }
+            }
+        } else {
+            val activeNetwork = cm.activeNetworkInfo
+            if (activeNetwork != null) {
+                // connected to the internet
+                if (activeNetwork.type == ConnectivityManager.TYPE_WIFI) {
+                    result = true
+                } else if (activeNetwork.type == ConnectivityManager.TYPE_MOBILE) {
+                    result = true
+                }
+            }
+        }
+        return result
     }
 
     @JvmStatic
